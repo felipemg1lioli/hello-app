aa512b1b41878cd7a5354b6b6d9ec5bdc7f1b338
felipemg1lioli <felipemonedagilioli@gmail.com>
2025-09-29 09:20:29 -0300

    Alteração de mensagem(5)

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "Bom Dia!!!  =====  Projeto CI/CD PB JUN - 2025  =====  Felipe Moneda Gilioli"
+    return "Bom Dia!!!  =====  Projeto CI/CD - PB JUN - 2025  =====  Felipe Moneda Gilioli"
